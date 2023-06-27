3fe5ba84f464cb66d7f1997e4b84b2748cfe3d7d
AlekseyCh59 <alekseych59@gmail.com>
2023-06-27 08:16:22 +0500

    добавил интерфейс и картинки

---
+++ Assets/Stats.cs
@@ -0,0 +1,22 @@
+
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class Stats : ScriptableObject
+{
+
+    public float healthPoints = 100f;
+    public float maxhp = 100f;
+    public float regeneration = 0;
+    public float level = 1;
+    public float experiens = 0;
+    public float bonusExp = 0;
+    public float Speed = 2;
+    public float MagicResistance = 0;
+    public float PhisicResistance = 0;
+    public float armor = 0;
+    public int money = 0;
+
+    public
+}
